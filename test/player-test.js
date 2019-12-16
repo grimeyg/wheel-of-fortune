5d5494531a228fde13c20fc7137100e0bda1bf53
kevin johnson <kevinj902302@gmail.com>
2019-12-16 16:32:28 -0700

    Added basic player tests

---
+++ test/player-test.js
@@ -0,0 +1,19 @@
+import chai from 'chai';
+const expect = chai.expect;
+import Player from '../classes/player.js';
+
+describe('Player', function () {
+  let player;
+
+  beforeEach(() => {
+    player = new Player();
+  });
+
+  it('should be a function', () => {
+    expect(Player).to.be.a('function');
+  });
+  
+  it('should be an instance of Player', () => {
+    expect(player).to.be.an.instanceof(Player);
+  });
+});
