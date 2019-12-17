af71e3c8a49159fbf84cb10c413c03c1fcfe5c08
Joel Lacey <joellaceybusiness@gmail.com>
2019-12-16 17:58:31 -0700

    Load puzzle data from API and store on instantiation

--- classes/game.js
+++ classes/game.js
@@ -1,23 +1,35 @@
 import Player from './player.js';
-
+// import Puzzle from './puzzle.js';
 
 class Game {
-    constructor() {
-        this.turn = 0;
-        this.players = [];
-        this.rounds = [];
-        this.puzzles = [];
+  constructor() {
+    this.turn = 0;
+    this.players = [];
+    this.rounds = [];
+    this.puzzles = [];
+  }
+
+  startGame(name1, name2, name3) {
+    if (this.players.length === 0) {
+      let player1 = new Player(name1);
+      let player2 = new Player(name2);
+      let player3 = new Player(name3);
+      this.players.push(player1, player2, player3);
     }
+  }
 
-   startGame(name1, name2, name3) {
-     if(this.players.length === 0) {
-       let player1 = new Player(name1);
-       let player2 = new Player(name2);
-       let player3 = new Player(name3);
-       this.players.push(player1, player2, player3);
-     };
-   };
- };
+  loadPuzzles() {
+    let allPuzzles = [];
+    fetch('https://fe-apps.herokuapp.com/api/v1/gametime/1903/wheel-of-fortune/data')
+      .then(response => response.json())
+      .then(data => Object.keys(data.data.puzzles).forEach(puzzleType => {
+        data.data.puzzles[puzzleType].puzzle_bank.forEach(puzzle => allPuzzles.push(puzzle))
+      }))
+      //should add an error handling alert 
+      .catch(err => console.log(err))
+    return allPuzzles;
+  }
+}
 
 
-  export default Game;
+export default Game;
--- src/scripts.js
+++ src/scripts.js
@@ -3,3 +3,7 @@ import Player from '../classes/player.js';
 import Puzzle from '../classes/puzzle.js';
 import Round from '../classes/round.js';
 import Wheel from '../classes/wheel.js';
+
+const game = new Game();
+
+console.log(game.loadPuzzles());
