77820f169ae8c0bced9ebd8e2a0e202be51518e5
kevin johnson <kevinj902302@gmail.com>
2020-01-07 17:18:12 -0700

    Added wheel tests

--- test/wheel-test.js
+++ test/wheel-test.js
@@ -1,7 +1,8 @@
 import chai from 'chai';
 const expect = chai.expect;
-import Wheel from '../classes/puzzle.js';
+import Wheel from '../classes/wheel.js';
 let wheel;
+
 
 
 describe('Wheel', function () {
@@ -17,6 +18,15 @@ describe('Wheel', function () {
 
   it('should be an instance of Wheel', function () {
     expect(wheel).to.be.an.instanceof(Wheel);
+  });
+
+  it('should be able to choose a value', function () {
+    expect(wheel.chooseValue()).to.be.within(0,19);
+  });
+
+  it('should be able to get a position', function () {
+    let index = wheel.chooseValue();
+    expect(wheel.getPosition(index)).to.be.within(3,4);
   });
 
 });
