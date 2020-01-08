5406b3d559ed526a48ee37aae596526c45166915
kevin johnson <kevinj902302@gmail.com>
2020-01-07 17:18:44 -0700

    Added bonusRound file

---
+++ classes/bonusRound.js
@@ -0,0 +1,15 @@
+import Player from './player.js';
+import Puzzle from './puzzle.js';
+import Round from './round.js';
+import $ from 'jquery';
+
+class BonusRound extends Round  {
+  constructor(puzzle) {
+    super(puzzle);
+  }
+
+}
+
+
+
+export default BonusRound;
